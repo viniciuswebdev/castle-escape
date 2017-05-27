99ad68f176094c9c4e2081c7742fc42e9318de42
Vinicius Oliveira <viniciuswebdev@gmail.com>
2017-05-27 18:57:57 -0300

    Add the player class and include in the game

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,7 @@
 window.addEventListener("load",function() {
   var Q = window.Q = Quintus({development: true})
     .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
+    .include("ActionPlatformerPlayer")
     .setup({
       width: 320,   //to fit devices with a screne resolution of 1280 x 720
       height: 180,
@@ -9,13 +10,13 @@ window.addEventListener("load",function() {
 
     Q.setImageSmoothing(false);
 
-    //define scene
-    Q.scene("level", function(stage){
-      Q.stageTMX("small_level.tmx", stage);
+    Q.scene("level",function(stage) {
+      Q.stageTMX("small_level.tmx",stage);      
     });
 
     //load assets
-    Q.loadTMX("small_level.tmx", function(){
+    Q.loadTMX("small_level.tmx, sprites.json, sprites.png", function() {       
+      Q.compileSheets("sprites.png","sprites.json");     
       Q.stageScene("level");
     });
     
