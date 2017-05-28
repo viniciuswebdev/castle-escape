5997707b06eca9301a6a4d4551c2afbd32a0e02e
Vinicius Oliveira <viniciuswebdev@gmail.com>
2017-05-27 23:52:26 -0300

    Add vertical movement enemy

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -10,10 +10,8 @@ Quintus.ActionPlatformerEnemy = function(Q) {
       });
       entity.on("bump.top",function(collision) {
         if(collision.obj.isA("Player")) { 
-          //make the player jump
           collision.obj.p.vy = -100;
 
-          //kill enemy
           this.destroy();
         }
       });
@@ -26,7 +24,7 @@ Quintus.ActionPlatformerEnemy = function(Q) {
       this.add("2d, aiBounce, commonEnemy");
     },
     step: function(dt) {        
-      var dirX = this.p.vx / Math.abs(this.p.vx);
+      var dirX = this.p.vx/Math.abs(this.p.vx);
       var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
       var nextElement = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
       var nextTile;
@@ -54,8 +52,36 @@ Quintus.ActionPlatformerEnemy = function(Q) {
         }
         this.p.vx = -this.p.vx;
       }
-
     }
-  });      
+  });  
+  
+  Q.Sprite.extend("VerticalEnemy", {
+    init: function(p) {
+      this._super(p, {vy: -100, rangeY: 40, gravity: 0 });
+      this.add("2d, commonEnemy");
+
+      this.p.initialY = this.p.y;
+      this.p.initialVy = this.p.vy;
+      this.p.vyDirection = this.p.vy/Math.abs(this.p.vy);
+
+      this.on("bump.top, bump.bottom",function(collision) {
+        that.p.vy = -Math.abs(that.p.initialVy) * that.p.vyDirection;
+        that.p.vyDirection = that.p.vy/Math.abs(that.p.vy);
+      });
+
+    },
+    step: function(dt) {                
+      if(this.p.y - this.p.initialY >= this.p.rangeY && this.p.vy > 0) {
+        this.p.vy = -this.p.vy;   
+        this.p.vyDirection *= -1;               
+      } 
+      else if(-this.p.y + this.p.initialY >= this.p.rangeY && this.p.vy < 0) {
+        this.p.vy = -this.p.vy;  
+        this.p.vyDirection *= -1;                 
+      } 
+    }
+  });
+
+  
   
 };
